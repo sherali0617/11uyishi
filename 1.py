475ccd8b60422c581ec74e015f6d1d38ebd04906
sherali0617 <begaliyevsherali625@gmail.com>
2026-05-13 17:15:41 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,47 @@
+import os
+os.system("cls")
+
+import json
+
+# d1 = {"a": 1, "b": 2, "c": 3}
+# d2 = {"b": 4, "d": 5}
+# natija=d1.copy()
+# for k,q in d2.items():
+#     if k in natija:
+#         natija[k]+=q
+#     else:
+#         natija[k]=q
+# print(natija)
+
+
+# 2
+# data = [("Ali", 85), ("Vali", 92), ("Sami", 77)]
+# natija=dict(data)
+# print(natija)
+
+
+# 3
+# with open("ism.json", "r",encoding="utf-8") as file:
+#     data=json.load(file)
+
+# for k in data:
+#     if k['age']>18:
+#         print(k)
+
+
+# 4.m
+# with open("ism.json","r") as f:
+#     data =json.load(f)
+# yangi={"name":"ali"},{"name":"vali"}
+# data.append(yangi)
+
+# with open ("ism.json","w") as file:
+#     json.dump(data,file,indent=4, ensure_ascii=False)
+
+
+
+
+
+
+
+
